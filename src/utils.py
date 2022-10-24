e009d801e35fbfc55200784c1b3e4a3fa8b8d1a2
pehcy <cheeyung.peh@gmail.com>
2022-10-24 11:22:10 +0800

    Update notebook for ML

--- src/utils.py
+++ src/utils.py
@@ -168,7 +168,7 @@ def map_feature(X1, X2):
     """
     X1 = np.atleast_1d(X1)
     X2 = np.atleast_1d(X2)
-    degree = 8
+    degree = 10
     out = []
     for i in range(1, degree+1):
         for j in range(i + 1):
